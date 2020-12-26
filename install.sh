9c169c87eda5dc9dc766ed457ff679fe135bcc4b
Fernando <fernando.augustogomesp@gmail.com>
2020-12-26 16:25:56 -0300

    Automatic install

---
+++ install.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/env bash
+
+FILE=~/.oh-my-bash
+if [ -d "$FILE" ];
+
+then
+	echo "You already have oh-my-bash"
+else
+	exec bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+fi
+
+FILE=/usr/bin/git
+
+if [ -f "$FILE" ];
+then 
+	echo "You already have git"
+else 
+	exec sudo apt install git
+fi
+
+FILE=~/.oh-my-bash/themes/sexy-wanelo
+
+if [ -d "$FILE" ];
+then 
+	echo "**********************************"
+	echo "* The theme is already installed *"
+	echo "**********************************"
+else 
+	git clone https://github.com/fernando-gap/sexy-wanelo.git ~/.oh-my-bash/themes/sexy-wanelo
+	echo ""
+	echo ""
+	echo "************************"
+	echo "* Succefull  installed *"
+	echo "************************"
+
+fi
+
+
